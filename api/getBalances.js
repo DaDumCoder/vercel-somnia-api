43fac389f123e27433c21d349f482e87300d844c
D-Dum <piyushankit1410@gmail.com>
2025-06-03 12:03:28 +0530

    Update getBalances.js

--- api/getBalances.js
+++ api/getBalances.js
@@ -1,6 +1,6 @@
 const { ethers } = require("ethers");
 
-const RPC_URL = "rpc.ankr.com/somnia_testnet/6e3fd81558cf77b928b06b38e9409b4677b637118114e83364486294d5ff4811";
+const RPC_URL = "https://rpc.ankr.com/somnia_testnet/6e3fd81558cf77b928b06b38e9409b4677b637118114e83364486294d5ff4811";
 const provider = new ethers.JsonRpcProvider(RPC_URL);
 
 // Contract addresses aligned with variables
